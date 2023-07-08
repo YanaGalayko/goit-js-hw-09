75999f1a270e3a919c4495b7a795570a15e47f48
YanaGalayko <117485236+YanaGalayko@users.noreply.github.com>
2023-07-08 11:24:17 +0000

    Deploying to gh-pages from @ YanaGalayko/goit-js-hw-09@f70af4a1937a45217ca549ac6be70733159302e5 🚀

---
+++ 01-color-switcher.95996dba.js
@@ -0,0 +1,2 @@
+const t={btnStart:document.querySelector("button[data-start]"),btnStop:document.querySelector("button[data-stop]"),body:document.body};let n=null;t.btnStart.addEventListener("click",(function(){t.btnStart.disabled=!0,t.btnStop.disabled=!1,n=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),t.btnStop.addEventListener("click",(function(){t.btnStop.disabled=!0,t.btnStart.disabled=!1,clearInterval(n)}));
+//# sourceMappingURL=01-color-switcher.95996dba.js.map
